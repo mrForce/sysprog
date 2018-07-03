fb48b24fc285da5067ab73036bd0219a671794ec
Jordan Force <jordan.force@uconn.edu>
2018-07-03 09:52:38 -0400

    It works!

--- replay.cpp
+++ replay.cpp
@@ -22,7 +22,31 @@ struct Entry{
 };
 
 
+void read_for_time(unsigned long long total_delay, std::ofstream& output_file, int read_pipe ){
+  auto start_time = std::chrono::high_resolution_clock::now();
+  
+  std::stringstream ss;
+  char* characters = (char*) malloc(100*sizeof(char));
+  std::vector<char> char_vec;
+  while(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() <= total_delay){
+    ssize_t num_bytes_read = read(read_pipe, characters, 99);
 
+    if(num_bytes_read > 0){
+      std::cout << "num bytes read: " << num_bytes_read << std::endl;
+      for(int i = 0; i < num_bytes_read; i++){
+	if(characters[i] > 0){
+	  char_vec.push_back(characters[i]);
+	}
+      }
+    }
+  }
+  free(characters);
+  std::cout << "Time spent reading: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << std::endl;
+  for(std::vector<char>::iterator iter = char_vec.begin(); iter != char_vec.end(); ++iter){
+    output_file << *iter;
+  }
+
+}
 
 
 std::vector<Entry> read_entry_file(std::string filename){
@@ -51,15 +75,17 @@ std::vector<Entry> read_entry_file(std::string filename){
 int main(int argc, char **argv)
 {
   /* 
-     Four args: entry file, multiplier, command,  output file
+     Five args: entry file, multiplier, time before stopping, command,  output file
   */
-  if(argc < 5){
-    std::cout << "Usage: ./replay entry_file_name multiplier exec_name (exec args) output_file_name" << std::endl;
+  if(argc < 6){
+    std::cout << "Usage: ./replay entry_file_name multiplier end_wait_time exec_name (exec args) output_file_name" << std::endl;
   }else{
     std::string entry_file_name(argv[1]);
     std::string multiplier_string(argv[2]);
+    std::string end_wait_string(argv[3]);
     int multiplier = std::stoi(multiplier_string);
-    std::string exec_name(argv[3]);
+    int end_wait_time = std::stoi(end_wait_string);
+    std::string exec_name(argv[4]);
     std::string output_file_name(argv[argc-1]);
     std::vector<Entry> entries = read_entry_file(entry_file_name);
 
@@ -84,7 +110,7 @@ int main(int argc, char **argv)
     if (pid == 0){
       //this is the child process
       //fd[0] is read end, fd[1] is the write end
-      std::cout << "Exec name: " << exec_name << std::endl;
+      //std::cout << "Exec name: " << exec_name << std::endl;
       close(parent_to_child[1]);
       close(child_to_parent[0]);
       dup2(parent_to_child[0], 0);
@@ -93,19 +119,20 @@ int main(int argc, char **argv)
       close(child_to_parent[1]);
       
       argv[argc - 1] = NULL;
-      std::cout << "Going to call execv" << std::endl;
+      //std::cout << "Going to call execv" << std::endl;
+      /*
         std::string line_one, line_two, line_three;
 	std::getline(std::cin, line_one);
-	std::cout << "Got first line" << std::endl;
+	//std::cout << "Got first line" << std::endl;
   std::getline(std::cin, line_two);
-  std::cout << "Second line" << std::endl;
+  //std::cout << "Second line" << std::endl;
   std::getline(std::cin, line_three);
-  std::cout << "Third line" << std::endl;
+  //  std::cout << "Third line" << std::endl;
   std::cout << line_three << std::endl;
   std::cout << line_two << std::endl;
-  std::cout << line_one << std::endl;
+  std::cout << line_one << std::endl;*/
 
-  //execv(exec_name.c_str(), &argv[3]);
+  execv(exec_name.c_str(), &argv[4]);
   //  std::cerr << "execv failed" << std::endl;
     }else if (pid > 0) {
       //std::vector<Entry> entries = read_entry_file(entry_file_name);
@@ -121,33 +148,20 @@ int main(int argc, char **argv)
 	std::string line = (iter->line).append("\n");
 	std::cout << "Line: " << line;
 	
-	
-	auto start_time = std::chrono::high_resolution_clock::now();
 	unsigned long long total_delay = delay*multiplier;
-	std::stringstream ss;
-	while(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() <= total_delay){
-	  char characters[100];
-	  ssize_t num_bytes_read = read(child_to_parent[0], characters, 99);
-	  characters[num_bytes_read] = '\0';
-	  if(num_bytes_read > 0){
-	    std::cout << "num bytes read: " << num_bytes_read << std::endl;
-	    std::string s(characters);
-	    std::cout << "string: " << s << std::endl;
-	    ss << s;
-	  }
-	}
-	std::cout << "Time spent reading: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << std::endl;
-	output_file << ss.str();
+	read_for_time(total_delay, output_file, child_to_parent[0]);
 	const char* c_string = line.c_str();
 	ssize_t num_bytes_written = 0;
-	num_bytes_written = write(parent_to_child[1], c_string, strlen(c_string) + 1);
-	std::cout << "Num bytes written: " << num_bytes_written << std::endl;
-	/*
+	//num_bytes_written = write(parent_to_child[1], c_string, strlen(c_string) + 1);
+	//std::cout << "Num bytes written: " << num_bytes_written << std::endl;
+	
 	while(num_bytes_written < strlen(c_string) + 1){
 	  num_bytes_written += write(parent_to_child[1], &c_string[num_bytes_written], strlen(&c_string[num_bytes_written]) + 1);
-	  }*/
+	  }
 	output_file << line;
       }
+      unsigned long long time_ull = (unsigned long long) end_wait_time;
+      read_for_time(time_ull*1000, output_file, child_to_parent[0]);
       output_file.close();
     }
   }
